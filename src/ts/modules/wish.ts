12a6e041329856a1bc1a60bee542b62158d90f6c
Arifullahkhan1 <arifullahkhan@hotmail.com>
2022-04-07 03:46:29 +0200

    final-todos

--- src/ts/modules/wish.ts
+++ src/ts/modules/wish.ts
@@ -1,32 +1,4 @@
-/* export class Wish{
-  index: number;
-  
- 
 
-  constructor(public wish:string){}
-  
-
-}
-  *//* export class TreeWish{
-    value:string;
-    
-    
-    map(_arg0: (el: TreeWish, i: TreeWish) => { index: TreeWish; value:TreeWish; }) {
-      throw new Error("Method not implemented.");
-    }
-    push(_value: string) {
-      throw new Error("Method not implemented.");
-    }
-    sort(_arg0: (a:TreeWish, b: TreeWish) => 0 | 1 | -1) {
-      throw new Error("Method not implemented.");
-    }
-     todos:Array <string>=[]  ; 
-
-  constructor(){};
- 
-   
-}   */
-        
 
 
 export class Wish{
